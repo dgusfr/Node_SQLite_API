eed13c45e7caf8765ee2d88f9428221f29773dca
dgusfr <diegogustavo.fr@gmail.com>
2024-12-13 09:23:12 -0300

    fix(update): pessoa controller

--- src/controllers/pessoaController.js
+++ src/controllers/pessoaController.js
@@ -1,12 +1,13 @@
-const { Pessoa } = require('../models');
+const PessoaServices = require('../services/pessoaService');
+const pessoaServices = new PessoaServices();
 
 class PessoaController {
   static async getAll(req, res) {
     try {
-      const pessoas = await Pessoa.findAll();
+      const pessoas = await pessoaServices.pegaTodosOsRegistros();
       res.status(200).json(pessoas);
     } catch (error) {
-      res.status(500).json({ error: 'Erro ao listar as pessoas.' });
+      res.status(500).json({ error: error.message });
     }
   }
 }
