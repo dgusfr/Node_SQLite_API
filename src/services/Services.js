ad8cf5c335173417ee4eef3aa4036d36e2021265
dgusfr <diegogustavo.fr@gmail.com>
2024-12-13 09:22:04 -0300

    feat(add(services)

---
+++ src/services/Services.js
@@ -0,0 +1,23 @@
+const dataSource = require('../models');
+
+class Services {
+  constructor(nomeDoModel) {
+    this.model = nomeDoModel;
+  }
+
+  async pegaTodosOsRegistros() {
+    return dataSource[this.model].findAll();
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    const listaDeRegistrosAtualizados = await dataSource[this.model].update(dadosAtualizados, {
+      where: { id: id }
+    });
+    if (listaDeRegistrosAtualizados[0] === 0) {
+      return false;
+    }
+    return true;
+  }
+}
+
+module.exports = Services;
