dcd4eb1dcbd12c56ba7b64ddfbc0ca97cd9e2d0b
dgusfr <diegogustavo.fr@gmail.com>
2024-12-13 09:55:13 -0300

    fix pdate service

--- src/app/services/Services.js
+++ src/app/services/Services.js
@@ -9,6 +9,10 @@ class Services {
     return dataSource[this.model].findAll();
   }
 
+  async createRecord(data) {
+    return dataSource[this.model].create(data);
+  }
+
   async updateRecord(updatedData, id) {
     const updatedRecordsList = await dataSource[this.model].update(updatedData, {
       where: { id: id }
@@ -17,6 +21,18 @@ class Services {
       return false;
     }
     return true;
+  }
+
+  async deleteRecord(id) {
+    const deleted = await dataSource[this.model].destroy({ where: { id: id } });
+    if (deleted === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  async getRecordById(id) {
+    return dataSource[this.model].findByPk(id);
   }
 }
 
