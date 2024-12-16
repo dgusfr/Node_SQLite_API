466aebf8849771971ff939424dd805628a6ee35e
dgusfr <diegogustavo.fr@gmail.com>
2024-12-15 21:23:24 -0300

    feat addd regiostrations seeders

--- src/database/seeders/20241216001653-demo-registration.js
+++ src/database/seeders/20241216001653-demo-registration.js
@@ -3,15 +3,37 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('People', [
+      {
+        estudante_id: 1,
+        curso_id: 1,
+        status: 'matriculado',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        estudante_id: 2,
+        curso_id: 2,
+        status: 'matriculado',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        estudante_id: 3,
+        curso_id: 3,
+        status: 'matriculado',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        estudante_id: 4,
+        curso_id: 4,
+        status: 'matriculado',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ], {});
+
   },
 
   async down (queryInterface, Sequelize) {
