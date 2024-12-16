8876b92547c9bf82d7cbb4d3c8ab6a9cc7063046
dgusfr <diegogustavo.fr@gmail.com>
2024-12-15 21:18:49 -0300

    feat(add): create seeds

---
+++ src/database/seeders/20241216001620-demo-categories.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('categories', [{
+      name: 'John Doe',
+      isBetaMember: false
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
