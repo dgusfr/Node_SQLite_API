ef0cabe9a201ea2a24256f7317f7c2c721aee6eb
dgusfr <diegogustavo.fr@gmail.com>
2024-12-15 21:13:58 -0300

    feat update

--- src/database/seeders/20241212180241-demo-pessoa.js
+++ src/database/seeders/20241212180241-demo-pessoa.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('pessoas', [
+    await queryInterface.bulkInsert('people', [
       {
         name: 'Solange Estudante',
         email: 'solange@email.com',
@@ -61,6 +61,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('pessoas', null, {});
+    await queryInterface.bulkDelete('people', null, {});
   }
 };
